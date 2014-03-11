404edd6282a0ffbc9b9239b744cc827ed726fad2
LasseBP <lasse@huhej.dk>
2014-03-11 17:37:17 +0100

    prototype of decoder added.

---
+++ TIINCO/Miniprojekt/Matlab/Decoder.m
@@ -0,0 +1,63 @@
+clc;
+clear;
+
+n = 7;
+k = 4;
+g = [1 1 0 1]; % g(x) = 1 + X + X^3
+r = [1 0 1 1 0 1 1];
+%r = [0 0 1 0 1 1 0];
+
+s = zeros(1, (n-k-1) + 1);
+buffer = r;
+
+for i = 0:n-1,
+    gate = s(end); %store msb for feedback calc
+    s = circshift(s,[0 1]); %shift syndrom 1 right
+    s(1) = r(n-i); %shift bit from r to s
+    
+    %calc and apply feedback
+    feedback = gate * g;
+    feedback(end) = []; %remove msb
+    s = mod(s + feedback, 2); %add feedback to syndrom
+end
+
+dmin = gfweight(g);
+t = floor((dmin-1)/2);
+
+%calc error patterns
+E = zeros(1, n);
+E(end) = 1;
+
+H = cyclgen(n,g,'system');
+
+S = E*H'; %calc all syndrome vectors
+
+errors = 0;
+synMod = 0;
+for i = 0:n-1,
+    gate = s(end); %store msb for feedback calc
+    s = circshift(s,[0 1]); %shift syndrom 1 right
+    s(1) = 0; %reset lsb to counter circshift() wrapping around
+    buffer = circshift(buffer,[0 1]); %shift syndrom 1 right    
+    
+    %calc and apply feedback
+    feedback = gate * g;
+    feedback(end) = []; %remove msb
+    s = mod(s + feedback, 2); %add feedback to syndrom
+    s(1) = mod(s(1) + synMod, 2); %apply syndrome modification
+    
+    if s == S %should be if s in S
+        buffer(end) = mod(buffer(end) + 1, 2);
+        synMod = 1; %syndrome modification
+        errors = errors + 1;
+    else
+        synMod = 0;
+    end
+end
+
+if s == zeros(1, (n-k-1) + 1) %if syndrome vector is all zeroes
+    %decoded correct
+else
+    %uncorrectable
+end
+
