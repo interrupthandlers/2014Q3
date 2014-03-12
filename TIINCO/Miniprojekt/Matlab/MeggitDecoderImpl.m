c06bc068f723f9d192f6c60cde9529ca5a4bdce5
LasseBP <lasse@huhej.dk>
2014-03-12 17:07:56 +0100

    auto 2 bit error patterns added. Decoder class almost implemented.

--- TIINCO/Miniprojekt/Matlab/MeggitDecoderImpl.m
+++ TIINCO/Miniprojekt/Matlab/MeggitDecoderImpl.m
@@ -1,6 +1,6 @@
 classdef MeggitDecoderImpl < handle
    properties (Hidden)
-      decodeIteration = 0;
+      synMod = 0;
    end
    properties (SetAccess = private)
       g; % generator poly
@@ -10,10 +10,11 @@ classdef MeggitDecoderImpl < handle
       s; % syndrome vector register
       buffer; %buffer
       correctedErrors = 0;
+      decodeIteration = 0;
       S; % all syndrome vectors corresponding to error patterns with 1 as highest bit
    end   
    methods(Access = public)
-      function MD = MeggitDecoderImpl(g, n, k)
+      function MD = MeggitDecoderImpl(g, n, k) %constructor
         MD.g = g;
         MD.n = n;
         MD.k = k;
@@ -23,13 +24,93 @@ classdef MeggitDecoderImpl < handle
         MD.r = r;
         MD.reset()              
       end
+      function decodeSingleStep(MD)
+        if MD.IsInited() == 0
+            error('Received vector is uninitialized.')
+        end
+          
+        if MD.IsDecodingComplete()
+            disp('Decoding is complete.')
+            return
+        end
+          
+        gate = MD.s(end); %store msb for feedback calc
+        MD.s = circshift(MD.s,[0 1]); %shift syndrom 1 right
+        MD.s(1) = 0; %reset lsb to counter circshift() wrapping around
+        MD.buffer = circshift(MD.buffer,[0 1]); %shift syndrom 1 right    
+
+        %calc and apply feedback
+        feedback = gate * MD.g;
+        feedback(end) = []; %remove msb
+        MD.s = mod(MD.s + feedback, 2); %add feedback to syndrom
+        MD.s(1) = mod(MD.s(1) + MD.synMod, 2); %apply syndrome modification
+
+        %check if the syndrome vector corresponds to an error pattern with 1 as highest bit
+        if ismember(MD.s, MD.S, 'rows') %if S contains a vector equal to s
+            MD.buffer(end) = mod(MD.buffer(end) + 1, 2); %correct the buffer
+            MD.synMod = 1; %syndrome modification
+            MD.correctedErrors = MD.correctedErrors + 1;
+        else
+            MD.synMod = 0;
+        end
+        MD.decodeIteration = MD.decodeIteration + 1;
+      end
+      function decodeFullStep(MD)
+          if MD.IsInited() == 0
+              error('Received vector is uninitialized.')
+          end
+          
+          while MD.IsDecodingComplete() == 0
+              MD.decodeSingleStep();
+          end
+          disp('Decoding is complete.')
+      end
+      function getStatus(MD)        
+        if MD.s == zeros(1, (MD.n-MD.k-1) + 1) %if syndrome vector is all zeroes
+            %decoded correct, maybe with errors
+        else
+            %uncorrectable
+        end
+      end
+      function isComplete = IsDecodingComplete(MD)
+          isComplete = MD.decodeIteration >= MD.n;
+      end
+      function isInited = IsInited(MD)
+          isInited = isempty(MD.r) == 0;
+      end
+   end % methods (public)
+   methods(Access = private)
+      function createSyndromeVectors(MD)
+        dmin = gfweight(MD.g);
+        t = floor((dmin-1)/2);
+        
+        %create one bit error pattern
+        E = zeros(1, MD.n);
+        E(end) = 1;
+      
+        if t > 1
+            %create 2 bit error patterns
+            for i = 1:MD.n-1
+                e = zeros(1, MD.n);
+                e(i) = 1; %set bit
+                e(end) = 1; %set MSb (all 2 pattern have MSb set)
+                E = [E ; e]; 
+            end
+        elseif t > 2
+            error('Number of error bits not supported.') %2 bits needs to be supported.
+        end
+        
+        H = cyclgen(MD.n,MD.g,'system');
+
+        MD.S = E*H'; %calc all syndrome vectors
+      end
       function reset(MD)
         MD.s = zeros(1, (MD.n-MD.k-1) + 1);
         MD.buffer = MD.r;
         MD.correctedErrors = 0;
         MD.decodeIteration = 0;
         
-        for i = 0:n-1,
+        for i = 0:MD.n-1,
             gate = MD.s(end); %store msb for feedback calc
             MD.s = circshift(MD.s,[0 1]); %shift syndrom 1 right
             MD.s(1) = MD.r(MD.n-i); %shift bit from r to s
@@ -39,35 +120,6 @@ classdef MeggitDecoderImpl < handle
             feedback(end) = []; %remove msb
             MD.s = mod(MD.s + feedback, 2); %add feedback to syndrom
         end
-      end
-      function decodeSingleStep(MD)
-      end
-      function decodeFullStep(MD)
-      end
-      function getStatus(MD)        
-        if s == zeros(1, (n-k-1) + 1) %if syndrome vector is all zeroes
-            %decoded correct
-        else
-            %uncorrectable
-        end
-      end
-   end % methods (public)
-   methods(Access = private)
-      function createSyndromeVectors(MD)
-        dmin = gfweight(MD.g);
-        t = floor((dmin-1)/2);
-        
-        %calc error patterns
-        if t == 1
-            E = zeros(1, n);
-            E(end) = 1;
-        else
-            error('Number of error bits not supported.')
-        end
-        
-        H = cyclgen(n,g,'system');
-
-        MD.S = E*H'; %calc all syndrome vectors
       end
    end % methods (private)
 end % classdef
---
+++ TIINCO/Miniprojekt/Matlab/errorPatternGeneration.m
@@ -0,0 +1,21 @@
+%create error pattern with one error bit
+n = 15;
+
+E = zeros(1, n);
+E(end) = 1;
+
+%create 2 bit error patterns
+for i = 1:n-1
+    e = zeros(1, n);
+    e(i) = 1; %set bit
+    e(end) = 1; %set MSb (all 2 pattern have MSb set)
+    E = [E ; e]; 
+end
+
+H = cyclgen(15, [1 0 0 0 1 0 1 1 1], 'system');
+
+S = [];
+
+for i = 1:size(E,1)
+    S = [S ; E(i,:)*H'];
+end
