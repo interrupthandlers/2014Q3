4a89935cdc3ede1dba7d98fe6f67980f8b2d62ca
ivangrujic <ivangrujic91@gmail.com>
2014-04-09 15:58:07 +0200

    Added gf3 and 5 elements

---
+++ TIINCO/Opgaver/gf3elements.m
@@ -0,0 +1,13 @@
+
+pi = 13,
+gfZero =    gf([0],3,pi),    
+gfOne =     gf([1],3,pi),
+a =         gf([2],3,pi),
+a2 =        gf([4],3,pi),
+a3 =        gf([5],3,pi),
+a4 =        gf([7],3,pi),
+a5 =        gf([3],3,pi),
+a6 =        gf([6],3,pi),
+
+
+
